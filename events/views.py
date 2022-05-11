09e508c5b55f94a8605ddac0723ad7407eb8d6e0
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-05-11 20:59:30 +0300

    file formatted

--- events/views.py
+++ events/views.py
@@ -18,28 +18,30 @@ class deneme(APIView):
 class CreateEvent(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permissions_classes = [permissions.IsAuthenticated]
+
     def post(self, request):
         try:
             Events.objects.create(
                 user=request.user, name=request.data['name'], date=request.data['date'])
         except:
-            return Response("",status=status.HTTP_404_NOT_FOUND)    
+            return Response("", status=status.HTTP_404_NOT_FOUND)
         return Response("", status=status.HTTP_201_CREATED)
+
 
 class ListEvents(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permissions_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        qs=Events.objects.all()
+        qs = Events.objects.all()
         return Response(qs.values())
+
 
 class DeleteEvents(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permissions_classes = [permissions.IsAuthenticated]
 
-    def delete(self, request,id):
-        obj=get_object_or_404(Events,id=id)
+    def delete(self, request, id):
+        obj = get_object_or_404(Events, id=id)
         obj.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)     
-        
+        return Response(status=status.HTTP_204_NO_CONTENT)
